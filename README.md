caff6130fbbf015059372ab27532f725fe219e07
Smita Bhat <smita.vivek.bhat@gmail.com>
2013-07-24 22:45:39 +0530

    Refactor and Add execution instructions in README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 package_dependency_checker
 ==========================
+
+>> git clone https://github.com/sbhat/package_dependency_checker.git
+>> cd package_dependency_checker
+>> bundle install
+>> bundle exec rspec .
+>> source=japa.parser dir=<project_dir>/package_dependency_checker/spec/src target=japa.parser.ast.* ruby main.rb
--- main.rb
+++ main.rb
@@ -3,9 +3,7 @@ $LOAD_PATH.unshift(ROOT)
 
 require 'package_dependency_checker.rb'
 
-source_packages = ENV['source']
-source_dirs = ENV['dir']
-target_packages = ENV['target']
+source_packages, source_dirs, target_packages = ENV['source'], ENV['dir'], ENV['target']
 if source_dirs.nil? || source_packages.nil? || target_packages.nil?
   puts "Please provide source directory(using 'dir=<source_dir>'), source package(using 'source=<source package>') and target packages(using 'target=<target packages>') to check for dependencies."
 else
