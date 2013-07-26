700f961d5984c8cbfc514b642725dec9c3edb732
Smita Bhat <smita.vivek.bhat@gmail.com>
2013-07-26 10:53:14 +0530

    Refactor.

--- app/java_source/package_tree.rb
+++ app/java_source/package_tree.rb
@@ -6,6 +6,29 @@ module JavaSource
       @root_package_nodes = root_package_nodes
     end
 
+    def self.create_by package_name
+      package_names = package_name.split('.')
+      return self.new if package_names.empty?
+
+      root_package = package_names.first
+      child_packages = (package_names[1..-1] || [])
+      new_root_package_node = JavaSource::PackageTreeNode.add(root_package, nil, child_packages, 0)
+
+      self.new([new_root_package_node])
+    end
+
+    def add package_name
+      package_names = package_name.split('.')
+      unless package_names.empty?
+        if matching_root_package_node = find_package_node_by_name_and_level(package_names.first, 0)
+          matching_root_package_node.add_child_nodes(package_names[1..-1] || [])
+        else
+          new_root_package_node = JavaSource::PackageTreeNode.add(package_names.first, nil, (package_names[1..-1] || []), 0)
+          @root_package_nodes << new_root_package_node unless new_root_package_node.nil?
+        end
+      end
+    end
+
     def find_package_node_by_name_and_level package_name, level = 0
       matching_package_node = nil
       @root_package_nodes.each do |root_package_node|
@@ -28,29 +51,6 @@ module JavaSource
       end
 
       self.class.new(new_root_package_nodes)
-    end
-
-    def self.create_by package_name
-      package_names = package_name.split('.')
-      return self.new if package_names.empty?
-
-      root_package = package_names.first
-      child_packages = (package_names[1..-1] || [])
-      new_root_package_node = JavaSource::PackageTreeNode.add(root_package, nil, child_packages, 0)
-
-      self.new([new_root_package_node])
-    end
-
-    def add package_name
-      package_names = package_name.split('.')
-      unless package_names.empty?
-        if matching_root_package_node = find_package_node_by_name_and_level(package_names.first, 0)
-          matching_root_package_node.add_child_nodes(package_names[1..-1] || [])
-        else
-          new_root_package_node = JavaSource::PackageTreeNode.add(package_names.first, nil, (package_names[1..-1] || []), 0)
-          @root_package_nodes << new_root_package_node unless new_root_package_node.nil?
-        end
-      end
     end
 
     def packages
