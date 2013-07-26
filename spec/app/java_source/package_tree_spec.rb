1e2040982b4456128b6421997aab319db902b70c
Smita Bhat <svb.19.2006@gmail.com>
2013-07-26 12:42:39 +0530

    Refactor

--- spec/app/java_source/package_tree_spec.rb
+++ spec/app/java_source/package_tree_spec.rb
@@ -7,23 +7,23 @@ describe JavaSource::PackageTree do
       @source_package_tree = JavaSource::PackageTree.create_by('japa.parser.ast')
     end
 
-    it "should return empty the package tree covers all its nodes using *" do
+    it "should return empty when the target package tree covers all its nodes using *" do
       target_package_tree = JavaSource::PackageTree.create_by('japa.*')
       (@source_package_tree - target_package_tree).packages.should be_empty
     end
 
-    it "should return all its packages when the package tree doesn't cover any of its packages" do
+    it "should return all its packages when the target package tree doesn't cover any of its packages" do
       target_package_tree = JavaSource::PackageTree.create_by('java.*')
-      (@source_package_tree - target_package_tree).packages.should == ['japa.parser.ast']
+      (@source_package_tree - target_package_tree).packages.should == @source_package_tree.packages
     end
 
-    it "should return empty when package tree covers all its packages" do
+    it "should return empty when the target package tree covers all its packages" do
       source_package_tree = JavaSource::PackageTree.create_by('japa.parser.Parser')
       target_package_tree = JavaSource::PackageTree.create_by('japa.parser')
-      (source_package_tree - target_package_tree).packages.should == []
+      (source_package_tree - target_package_tree).packages.should be_empty
     end
 
-    it "should return packages not covered by the package tree with no * child node" do
+    it "should return packages not covered by the target package tree when it has no * child node" do
       @source_package_tree.add('japa.parser.body.test')
       target_package_tree = JavaSource::PackageTree.create_by('japa.parser')
       (@source_package_tree - target_package_tree).packages.should == ['japa.parser.body.test']
@@ -34,9 +34,9 @@ describe JavaSource::PackageTree do
       (source_package_tree - target_package_tree).packages.should == ['japa.parser.body']
     end
 
-    it "should return empty when its packeges exactly match with the packages by target package tree" do
+    it "should return empty when its packeges exactly match with the packages of the target package tree" do
       target_package_tree = JavaSource::PackageTree.create_by('japa.parser.ast')
-      (@source_package_tree - target_package_tree).packages.should == []
+      (@source_package_tree - target_package_tree).packages.should be_empty
     end
   end
 end
